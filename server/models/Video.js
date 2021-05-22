1fafd54acc2f09908dd43863f2d46dcdcb612a17
yumichoi <yumichoi0921@gmail.com>
2021-05-22 20:00:05 +0900

    비디오 업로드

---
+++ server/models/Video.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const videoSchema = mongoose.Schema({
+   
+    writer: {
+        // Id로 User 모델의 모든 정보 사용
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    title: {
+        type: String,
+        maxlength: 50
+    },
+    description: {
+        type: String
+    },
+    privacy: {
+        type: Number
+    },
+    filePath: {
+        type: String
+    },
+    category: {
+        type: String
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    duration: {
+        type: String
+    },
+    thumbnail: {
+        type: String
+    }
+}, { timestamps: true }) // 생성된 날짜와 수정된 날짜 저장
+
+
+
+
+const Video = mongoose.model('Video', videoSchema);
+
+module.exports = { Video }
